6dec6cdcdb56b83ff49595a502dc06932a99ef32
888-coding <pachelee@gmail.com>
2023-09-01 10:52:47 -0300

    Criado Componentes

--- main.js
+++ main.js
@@ -1,41 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            onSale: true,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            seletedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 },
-            ]
+            cart: 0, 
+            premium: true
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.seletedVariant = index
-        }
-    },
-    computed: {
-        title(){
-            return this.brand + " " + this.product
-        },
-        image(){
-            return this.variants[this.seletedVariant].image
-        },
-        inStock(){
-            return this.variants[this.seletedVariant].quantity
-        },
-        sale(){
-            if (this.onSale){
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-    }
+    methods: {}
+    
 })
